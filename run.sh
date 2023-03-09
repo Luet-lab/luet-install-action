3c70373198df5d53ea6eb38d35dea479afa4413d
mudler <mudler@c3os.io>
2023-03-09 09:36:16 +0100

    First import

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+docker run --name luet quay.io/luet/base:"$LUET_TAG" && docker cp luet:/usr/bin/luet ./
+chmod +x luet
+sudo mv luet /usr/bin/luet
+sudo mkdir -p /etc/luet/repos.conf.d/
+sudo luet repo add -y kairos --url "$REPOSITORY" --type "$REPOSITORY_TYPE"
+LUET_NOLOCK=true sudo -E luet install -y "$PACKAGES"
